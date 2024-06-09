4e17de9965d47f79354e3416512f73525aefbc69
Jhopla <jorge.plaza.gj@gmail.com>
2024-06-08 20:34:52 -0400

    routes created, layout started

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { IndexComponent } from './components/index/index.component';
+import { LoginRegisterComponent } from './components/forms/login-register/login-register.component';
+
+export const routes: Routes = [
+    // General routes
+    { path: '', component: IndexComponent },
+    { path: 'login', component: LoginRegisterComponent },
+    { path: 'register', component: LoginRegisterComponent },
+];
