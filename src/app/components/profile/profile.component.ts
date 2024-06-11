42cc0f7d85a068ff723e9f26b1d0a915ac385639
Jhopla <jorge.plaza.gj@gmail.com>
2024-06-11 02:34:28 -0400

    profile page almost done

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faComment, faThumbsUp, faPaperPlane, faArrowTurnDown } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
+
 export class ProfileComponent {
+
+  faComment = faComment;
+  faThumbsUp = faThumbsUp;
+  faPaperPlane = faPaperPlane;
+  faArrowDown = faArrowTurnDown;
 
 }
